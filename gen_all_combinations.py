977e2f4a9ccffac04f4565d5d5180dc15f1f6c4a
pseudostripy <41550973+pseudostripy@users.noreply.github.com>
2021-07-07 18:09:18 +0100

    Remove trivial things from torture boards
     - Added trivial difficulty level
     - Changed difficulties of some objectives to trivial and moved order around
     - Updated the code to allow you to choose what to generate more easily
     - Updated the code to allow min/max levels for each generated board.

--- gen_all_combinations.py
+++ gen_all_combinations.py
@@ -2,14 +2,17 @@ from generate_bingo import generate
 from objectives import objectives
 
 # filter dictionary for the chosen specs
-difflevels = {"easy":   0,
-              "medium": 1,
-              "hard":   2,
-              "expert": 3,
-              "torture": 4
-              }
+catinfo = [{"name": "trivial",  "difficulty": 0, "mindifficulty": 0},
+           {"name": "easy",     "difficulty": 1, "mindifficulty": 0},
+           {"name": "medium",   "difficulty": 2, "mindifficulty": 0},
+           {"name": "hard",     "difficulty": 3, "mindifficulty": 0},
+           {"name": "expert",   "difficulty": 4, "mindifficulty": 0},
+           {"name": "torture",  "difficulty": 5, "mindifficulty": 1},
+           ]
 
-for key in difflevels.keys():
-    generate(objectives, difflevels, key, True)   # pre-drangleic tasks only
-    generate(objectives, difflevels, key, False)  # any tasks
+# Generate these types:
+genboards = ["easy", "medium", "hard", "expert", "torture"]
 
+for board in genboards:
+    generate(objectives, catinfo, board, True)   # pre-drangleic tasks only
+    generate(objectives, catinfo, board, False)  # any tasks
--- generate_bingo.py
+++ generate_bingo.py
@@ -1,11 +1,13 @@
-def generate(objectives, difflevels, thisdiff, bpredrang):
+def generate(objectives, catinfo, thisdiff, bpredrang):
     if bpredrang:
         suff = "predrang"
     else:
         suff = "all"
 
     # first filter (by difficulty level)
-    f1obj = [item for item in objectives if difflevels[item["level"]] <= difflevels[thisdiff]]
+    minlevel = getlevel(catinfo, thisdiff, "mindifficulty")
+    maxlevel = getlevel(catinfo, thisdiff)
+    f1obj = [item for item in objectives if maxlevel >= getlevel(catinfo, item["level"]) >= minlevel]
     if bpredrang:
         # sub-filter by predrangleic
         f1obj_props = [item for item in f1obj if "props" in item.keys()]
@@ -26,3 +28,9 @@ def generate(objectives, difflevels, thisdiff, bpredrang):
     # fix end of file format
     f.write("\n]\n")
     f.close()
+
+
+def getlevel(catinfo, strdiff, lookuptype="difficulty"):
+    for idx, dic in enumerate(catinfo):
+        if dic["name"] == strdiff:
+            return dic[lookuptype]
